f42a8ed4594c44104c8baaf4a9ac3d97d02ccb14
arrowsmith001 <arrowsmithalexander@gmail.com>
2023-09-21 01:26:28 +0100

    sonarcloud config 4

--- src/test/java/com/arrowsmith/sakiladb/SakilaDbApplicationTests.java
+++ src/test/java/com/arrowsmith/sakiladb/SakilaDbApplicationTests.java
@@ -1,14 +1,32 @@
 package com.arrowsmith.sakiladb;
 
+import com.arrowsmith.sakiladb.controllers.CategoryController;
+import com.arrowsmith.sakiladb.controllers.FilmController;
+import com.arrowsmith.sakiladb.entities.Category;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SakilaDbApplicationTests {
 
+	@Autowired
+	FilmController filmController;
+
+	@Autowired
+	CategoryController categoryController;
+
+
 	@Test
 	void contextLoads() {
 
 	}
+
+	@Test
+	void canCreateCategory() {
+		Category category = new Category();
+		categoryController.saveCategory(category);
+	}
+
 
 }
